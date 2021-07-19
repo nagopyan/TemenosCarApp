88db912743a2d2defb259c2b6fb06e9dcf7c1a0f
vpanayotovv <vvpanayotovv@gmail.com>
2021-07-19 19:20:46 +0300

    added db when landing page load and fix bug

--- controllers/mobile/frmLandingSearchScreenController/frmLandingSearchScreenControllerActions.js
+++ controllers/mobile/frmLandingSearchScreenController/frmLandingSearchScreenControllerActions.js
@@ -3,12 +3,6 @@ define({
       This is an auto generated file and any modifications to it may result in corruption of the action sequence.
     */
     /** onTouchStart defined for SearchForm **/
-    AS_UWI_hdbbdea3798a452f850714ed655ca215: function AS_UWI_hdbbdea3798a452f850714ed655ca215(eventobject, x, y) {
-        var self = this;
-        var ntf = new kony.mvc.Navigation("frmSearchList");
-        ntf.navigate();
-    },
-    /** onTouchStart defined for SearchForm **/
     AS_UWI_f6a8c4044c37430d91bc59b2fbea453e: function AS_UWI_f6a8c4044c37430d91bc59b2fbea453e(eventobject, x, y) {
         var self = this;
         var ntf = new kony.mvc.Navigation("frmSearchList");
