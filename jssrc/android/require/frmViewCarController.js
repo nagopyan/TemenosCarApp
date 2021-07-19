463d1334ef2d37122c31475a4105b4a09a188c2f
vpanayotovv <vvpanayotovv@gmail.com>
2021-07-19 16:43:23 +0300

    add functionality in search form and view car form

--- jssrc/android/require/frmViewCarController.js
+++ jssrc/android/require/frmViewCarController.js
@@ -1,5 +1,16 @@
 define("userfrmViewCarController", {
-    //Type your controller code here 
+    onViewCreated: function() {
+        this.view.preShow = this.preShow;
+    },
+    preShow: function() {
+        var offer = kony.store.getItem("OfferDetails");
+        this.view.carImage.src = offer.imgUrl;
+        this.view.lblOfferTitle.text = offer.title;
+        this.view.lblMake.text = offer.make;
+        this.view.lblModel.text = offer.model;
+        this.view.lblYear.text = offer.year;
+        this.view.lblFuelType = offer.fuel;
+    }
 });
 
 define("frmViewCarController", ["userfrmViewCarController", "frmViewCarControllerActions"], function() {
