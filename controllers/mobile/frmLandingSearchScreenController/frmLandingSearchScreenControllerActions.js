d435623ba9d364d6ab6a6b354e8253a94181db4a
vpanayotovv <vvpanayotovv@gmail.com>
2021-07-19 17:54:06 +0300

    add back btn

--- controllers/mobile/frmLandingSearchScreenController/frmLandingSearchScreenControllerActions.js
+++ controllers/mobile/frmLandingSearchScreenController/frmLandingSearchScreenControllerActions.js
@@ -7,5 +7,11 @@ define({
         var self = this;
         var ntf = new kony.mvc.Navigation("frmSearchList");
         ntf.navigate();
+    },
+    /** onTouchStart defined for SearchForm **/
+    AS_UWI_f6a8c4044c37430d91bc59b2fbea453e: function AS_UWI_f6a8c4044c37430d91bc59b2fbea453e(eventobject, x, y) {
+        var self = this;
+        var ntf = new kony.mvc.Navigation("frmSearchList");
+        ntf.navigate();
     }
 });
--- controllers/mobile/frmSearchListController/frmSearchListController.js
+++ controllers/mobile/frmSearchListController/frmSearchListController.js
@@ -7,6 +7,7 @@ define({
       fuel: "Benzin",
       imgUrl:"https://w7.pngwing.com/pngs/656/420/png-transparent-audi-a3-audi-s3-audi-quattro-audi-sportback-concept-audi-compact-car-car-mode-of-transport.png",
       year:"2016",
+      description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
       user: "",
     },
     {
@@ -16,6 +17,7 @@ define({
       fuel: "Dizel",
       imgUrl:"https://e7.pngegg.com/pngimages/240/386/png-clipart-toyota-sprinter-carib-toyota-supra-toyota-land-cruiser-prado-toyota-compact-car-car.png",
       year:"2016",
+      description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
       user: "",
     },{
       title: "Selling Opel kadet",
@@ -24,6 +26,7 @@ define({
       fuel: "Gaz",
       imgUrl:"https://img2.pngdownload.id/20180611/ibc/kisspng-opel-kadett-chevrolet-kadett-opel-astra-car-opel-kadett-a-5b1f0ea2ed7de2.4341901415287620189728.jpg",
       year:"2016",
+      description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
       user: "",
     }
   ],
@@ -40,13 +43,13 @@ define({
     this.view.btnChange.onClick = this.navigate;
     this.view.OfferList.SegmentListOffers.onRowClick = this.onRowClicked;
   },
-  
+
   onRowClicked:function(seguiWidget, sectionNumber, rowNumber, selectedState){
     kony.store.setItem("OfferDetails", this.OffersDb[rowNumber]);
-    var ntf = new kony.mvc.Navigation("frmViewCar");
+    var ntf = new kony.mvc.Navigation("frmViewManageOffer");
     ntf.navigate();
   },
-  
+
   navigate:function(){
     var nav = new kony.mvc.Navigation("frmLandingSearchScreen");
     nav.navigate();
