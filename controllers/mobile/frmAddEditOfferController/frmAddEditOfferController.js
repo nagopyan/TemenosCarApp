d044156e4c1a9eedefbe6c3452fd4fa8251fa855
Rayko Bozhurin <rbozhurin@gmail.com>
2021-07-21 16:08:26 +0300

    update Add Button

--- controllers/mobile/frmAddEditOfferController/frmAddEditOfferController.js
+++ controllers/mobile/frmAddEditOfferController/frmAddEditOfferController.js
@@ -2,27 +2,45 @@ define({
 
   onViewCreated: function() {
     this.view.preShow = this.preShow;
+    this.view.postShow = this.postShow;
   },
 
   preShow: function() {
     
-    var previousForm = kony.application.getPreviousForm.id;
-    if(previousForm === "frmViewOffers"){
-      this.view.AddSaveBtn.text = "Add";
-    }
+//     var previousForm = kony.application.getPreviousForm.id;
+//     if(previousForm === "frmViewOffers"){
+//       this.view.AddSaveBtn.text = "Add";
+//     }
+  },
+  
+  postShow: function() {
+    this.view.AddSaveBtn.onTouchStart = this.pullInfo;
+  },
+  
+  pullInfo: function() {
+    var carInfo = 
+        {
+          carTitle: this.view.TextFieldOfferTitle.text,
+          carImg: this.view.TextFieldImageUrl.text,
+          carMake: this.view.DropDownCarMake.selectedKeyValue[1],
+          carModel: this.view.DropDownCarModel.selectedKeyValue[1],
+          carFuel: this.view.DropDownFuelType.selectedKeyValue[1],
+          
+        };
+    alert("Make: " + carInfo.carMake);
   },
 
-  onNavigate: function(obj) {
-    if(kony.application.getPreviousForm.id === "frmViewManageOffer"){
+//   onNavigate: function(obj) {
+//     if(kony.application.getPreviousForm.id === "frmViewManageOffer"){
 
-      this.view.AddSaveBtn.text = "Save";
+//       this.view.AddSaveBtn.text = "Save" ;
 
-      this.view.TextFieldOfferTitle.text = obj.title;
-      this.view.TextFieldImageUrl.text = obj.imgUrl;
+//       this.view.TextFieldOfferTitle.text = obj.title;
+//       this.view.TextFieldImageUrl.text = obj.imgUrl;
 
-    }else{
-      this.view.AddSaveBtn.text = "Add";
-    }
-  }
+//     }else{
+//       this.view.AddSaveBtn.text = "Add";
+//     }
+//   }
 
 });
